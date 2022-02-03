9dc55c1262dbf780a1fa2625b3e0b18afa1b62c6
rwm774s@hotmail.com <rwm774s@hotmail.com>
2022-02-02 22:47:13 -0600

    Initial commit

---
+++ src/main/java/org/robertwmurphy/addresskeeperservice/AddresskeeperServiceApplication.java
@@ -0,0 +1,13 @@
+package org.robertwmurphy.addresskeeperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddresskeeperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddresskeeperServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/robertwmurphy/addresskeeperservice/AddresskeeperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.robertwmurphy.addresskeeperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddresskeeperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
